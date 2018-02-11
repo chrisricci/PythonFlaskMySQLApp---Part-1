5520cf5916c4111bfccfb1e2e3466f5a5b6e911f
Chris Ricci <chris.ricci@coreos.com>
2018-02-11 18:27:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,12 @@ app = Flask(__name__)
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = os.environ.get('MYSQL_DATABASE_USER')
+app.logger.debug('MYSQL_DATABASE_USER: ' + os.environ.get('MYSQL_DATABASE_USER'))
 app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get('MYSQL_DATABASE_PASSWORD')
 app.config['MYSQL_DATABASE_DB'] = os.environ.get('MYSQL_DATABASE_DB')
+app.logger.debug('MYSQL_DATABASE_DB: ' + os.environ.get('MYSQL_DATABASE_DB'))
 app.config['MYSQL_DATABASE_HOST'] = os.environ.get('MYSQL_DATABASE_HOST')
+app.logger.debug('MYSQL_DATABASE_HOST: ' + os.environ.get('MYSQL_DATABASE_HOST'))
 mysql.init_app(app)
 
 
