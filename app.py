dcd51ba18d00d3ef39bb55975f6dd85392ffafb7
Chris Ricci <chris.ricci@coreos.com>
2018-02-27 16:55:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app.config['MYSQL_DATABASE_DB'] = os.environ.get('MYSQL_DATABASE_DB') if os.envi
 sys.stdout.write(os.environ.get('MYSQL_DATABASE_DB'))
 app.config['MYSQL_DATABASE_HOST'] = os.environ.get('MYSQL_DATABASE_HOST') if os.environ.get('MYSQL_DATABASE_HOST') != None else 'localhost1'
 sys.stdout.write(os.environ.get('MYSQL_DATABASE_HOST'))
+sys.stdout.flush()
 mysql.init_app(app)
 
 @app.route('/')
