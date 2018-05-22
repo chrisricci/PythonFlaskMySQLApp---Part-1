3044ed0553605b11e4ee234210e1150468190a02
Chris Ricci <chris.ricci@coreos.com>
2018-05-22 10:37:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from flask import Flask, render_template, json, request
-from flask.ext.mysql import MySQL
+from flask_mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
 mysql = MySQL()
